5afe0f85db1e1bdc26d141787a6af239583fee19
Sr.Patsu21 <paulosergiopierdona@gmail.com>
2025-03-20 14:06:50 -0300

    db is running

--- mongo_script/shard-setup.js
+++
@@ -1,4 +0,0 @@
-sh.addShard("shard1ReplSet/mongo_shard1-1:27018");
-sh.addShard("shard1ReplSet/mongo_shard2-1:27018");
-sh.enableSharding("test");
-sh.shardCollection("test.users", { userId: "hashed" });
