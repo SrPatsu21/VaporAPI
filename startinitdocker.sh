4f205e7e20f7184849cc921ea9aced0a7c9ff4e8
Sr.Patsu21 <paulosergiopierdona@gmail.com>
2025-04-29 23:11:18 -0300

    remove trash

--- startinitdocker.sh
+++
@@ -1,5 +0,0 @@
-docker compose -f docker-compose.db.yml up -d
-
-docker compose wait mongo-init-db
-
-docker compose -f docker-compose.web.yml up -d
--- stopdocker.sh
+++
@@ -1,3 +0,0 @@
-docker compose -f docker-compose.db.yml down
-
-docker compose -f docker-compose.web.yml down -v
